3cc46ea31d30c16b8645667bd2110e6ab93f6076
Dumitru C <canteadumitru@gmail.com>
2020-08-02 19:49:56 +0300

    check-data

---
+++ src/check-data.ts
@@ -0,0 +1,32 @@
+import fetch from "node-fetch";
+import { getSupportedCountries, getDataNames, getData } from "./index";
+import { SelectorsData } from "./types";
+
+const countries = getSupportedCountries();
+
+const checkData = async (data: SelectorsData) => {
+  for (const { list } of data.lists) {
+    let result = "✓";
+    try {
+      const { status } = await fetch(list.url);
+      result = status === 200 ? result : status.toString();
+    } catch (error) {
+      result = error.message;
+    }
+    console.log(`\t\t- ${list.url}: ${result}`);
+  }
+};
+
+async function check() {
+  for (const country of countries) {
+    console.log(`country ${country}:`);
+    const names = getDataNames({ country });
+    for (const name of names) {
+      console.log(`\t${name}:`);
+      const data = await getData({ country, name });
+      await checkData(data);
+    }
+  }
+}
+
+check();
